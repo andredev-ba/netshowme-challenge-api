1496d0d862c5d82da6a762ce0506f3ded6138c96
André Souza <andre.ti.bahia@gmail.com>
2021-05-21 10:11:16 -0300

    feat: add contact model

---
+++ app/Domain/Contact/Models/Contact.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Domain\Contact\Models;
+
+class Contact
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'message',
+        'file_path'
+    ];
+
+}
